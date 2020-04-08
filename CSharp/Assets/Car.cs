6647e7022c371ab63e116f651af270eb75c74733
gamer40930 <63327386+gamer40930@users.noreply.github.com>
2020-04-08 15:30:17 +0800

    summary

--- CSharp/Assets/Car.cs
+++ CSharp/Assets/Car.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 
 // 註解:用來說明、紀錄、筆記等..只會在腳本內看到
-// 摘要(三條線) summary; 描述此內容,會在其他腳本內看到 - 提高維護性與擴充性
+// 摘要(三條線) summary; 描述此內容,會在其他腳本內看到 - 提高維護性、擴充性、方便好讀!!
+// 新增的 C# 放在專案內為藍圖
+// 放在場景物件上為實體物件
 /// <summary>
 /// 定義汽車物件
 /// </summary>
@@ -15,14 +17,42 @@ public class Car : MonoBehaviour
     // 剎車        - 是否開放      - 布林值 bool  
 
     // 定義欄位於法
-    // 資料類型 欄位名稱 (指定 值);
+    // 修飾詞 資料類型 欄位名稱 (指定 值);
+    // 1.公開 public (會顯示在屬性面板, 允許其他類別忖取)
+    // 私人 praviate (預設) 不顯示, 不允許
     // 預設值 int、float 為0
     // string 為 ""
     // bool 為 false
-    int cc = 2000;  //點int +F12 可以看官方數值規範
-    float weight = 100.9f; 
-    string brand = "BMW"; 
-    bool brake = false ;
+    // 欄位屬性 Attribute : Unity 用於輔助欄位的功能
+    // 語法 : [屬性名稱(值)]
+    // 標題 [Header("字串")]
+    // 小提示 [Tooltip("字串")] - 游標停留時顯示
+    // 範圍 [Range(最小值, 最大值] - 僅限於數值型資料 int、float
+    
+
+    /// <summary>
+    /// 汽車CC數
+    /// </summary>
+    [Header("汽車CC數"), Tooltip ("CC數"), Range(1000, 10000)]
+    public int cc = 2000;  // 點int + F12 可以看官方數值規範
+
+    /// <summary>
+    /// 汽車重量
+    /// </summary>
+    [Header("汽車重量"), Tooltip("這格是存放汽車噸位用"), Range(1000, 10000)]
+    public float weight = 100.9f;
+
+    /// <summary>
+    /// 汽車品牌
+    /// </summary>
+    [Header("汽車品牌"), Tooltip("這台汽車的品牌")]
+    public string brand = "BMW";
+
+    /// <summary>
+    /// 是否煞車
+    /// </summary>
+    [Header("是否煞車"), Tooltip("用來儲存目前車子是否為煞車狀態")]
+    public bool brake = false ;
 
 
 
--- CSharp/Assets/Class01.cs
+++ CSharp/Assets/Class01.cs
@@ -11,11 +11,40 @@ public class Class01 : MonoBehaviour
     // 1.大小寫不一樣
     // 2.括號成對出現 () {} [] '' "" < >
 
-    //事件:特定時間會已指定次數執行的方法 (名稱為藍色)
+    [Header ("Car01")]
+    public Car car01; //招喚Car的腳本資料 & 使用它
 
-    private void Start()//開始事件:播放遊戲時執行一次 - 初始事件
+    [Header("Car02")]
+    public Car car02 ; //招喚Car的腳本資料 & 使用它
+
+
+    //事件:特定時間會已指定次數執行的方法 (名稱為藍色)
+    //開始事件:播放遊戲時執行一次 - 初始事件
+    private void Start()
     {
-        print("HI WORLD!"); //輸出("文字訊息")
+        //輸出("文字訊息")
+        print("HI WORLD!"); 
+
+        // 取得 get
+
+        print("car01的CC數是" + car01.cc);
+        print("car01的品牌是" + car01.brand);
+        print("car02的品牌是" + car02.brand);
+
+        // 設定 set
+        //移動程式碼快捷鍵 ALT + 上下
+        car01.cc = 9999;
+        car01.weight = 2000.55f;
+        car01.brand = "Bently";
+        car01.brake = true;
+
+
+
+
+
     }
+
+
+
 
 }
